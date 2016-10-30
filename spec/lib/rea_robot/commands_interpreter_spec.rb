6995a4df0cac022560c3d2567f4a6a5d3bc369ed
Fredrik Persen Fostvedt <fpfostvedt@gmail.com>
2016-10-30 23:44:33 +1100

    add specs for CommandsInterpreter single commands received

--- spec/lib/rea_robot/commands_interpreter_spec.rb
+++ spec/lib/rea_robot/commands_interpreter_spec.rb
@@ -8,26 +8,52 @@ describe CommandsInterpreter do
   describe '#process' do
     subject(:process) { commands_interpreter.process }
 
-    describe 'problem examples' do
-      describe 'example a' do
-        let(:commands_string) do
-          <<-eos
+    after { process }
+
+    context 'mutliple commands' do
+      let(:commands_string) do
+        <<-eos
 PLACE 0,0,NORTH
 LEFT
 RIGHT
 MOVE
 REPORT
-          eos
-        end
+        eos
+      end
 
-        specify do
-          expect(robot).to receive(:place).with(0,0,'NORTH').ordered
-          expect(robot).to receive(:turn_left).ordered
-          expect(robot).to receive(:turn_right).ordered
-          expect(robot).to receive(:move_forward).ordered
-          expect(robot).to receive(:report).ordered
-          process
-        end
+      it 'receives any command in correct order' do
+        expect(robot).to receive(:place).with(0,0,'NORTH').ordered
+        expect(robot).to receive(:turn_left).ordered
+        expect(robot).to receive(:turn_right).ordered
+        expect(robot).to receive(:move_forward).ordered
+        expect(robot).to receive(:report).ordered
+      end
+    end
+
+    context 'single commands' do
+      describe 'PLACE' do
+        let(:commands_string) { 'PLACE 0,0,NORTH' }
+        specify { expect(robot).to receive(:place).with(0,0,'NORTH') }
+      end
+
+      describe 'LEFT' do
+        let(:commands_string) { 'LEFT' }
+        specify { expect(robot).to receive(:turn_left) }
+      end
+
+      describe 'RIGHT' do
+        let(:commands_string) { 'RIGHT' }
+        specify { expect(robot).to receive(:turn_right) }
+      end
+
+      describe 'MOVE' do
+        let(:commands_string) { 'MOVE' }
+        specify { expect(robot).to receive(:move_forward) }
+      end
+
+      describe 'REPORT' do
+        let(:commands_string) { 'REPORT' }
+        specify { expect(robot).to receive(:report) }
       end
     end
   end
