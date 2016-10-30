2c1d2c80a03f741fc1b4276444e70f781686045f
Fredrik Persen Fostvedt <fpfostvedt@gmail.com>
2016-10-30 16:59:18 +1100

    add Robot#move_forward specs

--- spec/lib/rea_robot/robot_spec.rb
+++ spec/lib/rea_robot/robot_spec.rb
@@ -20,12 +20,59 @@ describe Robot do
     end
   end
 
-  # facing north, moves forward +1 y
-  # facing east,  moves forward +1 x
-  # facing south, moves forward -1 y
-  # facing west,  moves forward -1 x
-  # can't move off north edge
-  # can't move off east edge
-  # can't move off south edge
-  # can't move off west edge
+  describe '#move_forward' do
+    subject(:move_forward) { robot.move_forward }
+
+    before do
+      robot.place(x, y, direction)
+      move_forward
+    end
+
+    context 'placed where can always move (middle)' do
+      let(:x) { 3 }
+      let(:y) { 3 }
+
+      context 'when direction is NORTH' do
+        let(:direction) { 'NORTH' }
+
+        specify { expect(robot.x).to eq x }
+        specify { expect(robot.y).to eq y + 1 }
+      end
+
+      context 'when direction is EAST' do
+        let(:direction) { 'EAST' }
+
+        specify { expect(robot.x).to eq x + 1 }
+        specify { expect(robot.y).to eq y }
+      end
+
+      context 'when direction is SOUTH' do
+        let(:direction) { 'SOUTH' }
+
+        specify { expect(robot.x).to eq x }
+        specify { expect(robot.y).to eq y - 1 }
+      end
+
+      context 'when direction is WEST' do
+        let(:direction) { 'WEST' }
+
+        specify { expect(robot.x).to eq x - 1 }
+        specify { expect(robot.y).to eq y }
+      end
+    end
+
+    context 'placed on edge facing edge' do
+      # can't move off north edge
+      # can't move off east edge
+      # can't move off south edge
+      # can't move off west edge
+    end
+  end
+
+  describe '#turn_right' do
+  end
+
+  describe '#turn_left' do
+  end
+
 end
